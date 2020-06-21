20a066e227186323e662c352fa43f5fa00755188
yiwen song <songzgy@gmail.com>
2020-06-21 01:37:34 -0700

    ledger reader improvements

--- ledger_reader/main.py
+++ ledger_reader/main.py
@@ -15,7 +15,7 @@ YEAR = 2020
 DATE_REGEX = re.compile(r"^(?P<month>\d\d?)/(?P<day>\d\d?)$")
 # How do you get PL from the lines
 PL_REGEX = re.compile(
-    r"^(?P<name>[^\(\)]*)( \(.*\))?: (?P<sign>-?)\$(?P<amount>[\d.]+)$"
+    r"^(?P<name>[^\(\)]*)( \(.*\))?: ?(?P<sign>-?)\$(?P<amount>[\d.]+)$"
 )
 
 
@@ -36,9 +36,13 @@ def get_day_to_day_results(
                 f"{YEAR}-{is_date.group('month')}-{is_date.group('day')}",
             ).isoformat()[:10]
         else:
+            # Skip lines for requesting money
+            if "requests" in line.lower():
+                continue
+
             match = PL_REGEX.match(line)
             if match is not None:
-                name = match.group("name")
+                name = match.group("name").lower()
                 sign = -1 if match.group("sign") == "-" else 1
                 amount = float(match.group("amount")) * sign
 
@@ -55,6 +59,11 @@ def get_results(path: str) -> None:
         day_to_day_results = get_day_to_day_results(result_file)
 
     results_df = pandas.DataFrame(day_to_day_results).fillna(0)
+
+    sums = results_df.sum(axis="columns")
+    results_df.insert(0, "Sum", sums)
+    results_df = results_df.sort_values("Sum", ascending=False)
+
     return results_df
 
 
@@ -105,6 +114,12 @@ def parse_arguments() -> argparse.Namespace:
         help="Disables uploading to google sheets",
     )
 
+    parser.add_argument(
+        "--refresh-google-api-auth",
+        action="store_true",
+        help="Refreshes the google api auth token",
+    )
+
     args = parser.parse_args()
 
     return args
@@ -124,7 +139,9 @@ def main() -> None:
 
     if not args.disable_sheets_upload:
         client = sheets_api.get_client(
-            args.google_credentials, args.google_credential_cache,
+            args.google_credentials,
+            args.google_credential_cache,
+            refresh_cache=args.refresh_google_api_auth,
         )
         sheets_api.upload_dataframe(client, args.sheet, results)
 
--- ledger_reader/sheets_api/__init__.py
+++ ledger_reader/sheets_api/__init__.py
@@ -1,4 +1,5 @@
 import pickle
+import typing
 
 import googleapiclient.discovery
 import pandas
@@ -18,17 +19,37 @@ def upload_dataframe(client, sheet_id: str, dataframe: pandas.DataFrame,) -> Non
     ).execute()
 
 
-def get_client(conf_path: str, cred_cache: str):
+def download_ledger(client, ledger_doc_id: str) -> typing.IO:
+    """Creates a stream with the ledger contents."""
+    doc = client.documents()
+    ledger_doc = doc.get(documentId=ledger_doc_id).execute()
+
+    return ledger_doc.get("body")
+
+
+def _make_new_client(conf_path: str, cred_cache: str):
+    cred_flow = flow.InstalledAppFlow.from_client_secrets_file(
+        conf_path,
+        [
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/documents",
+        ],
+    )
+    creds = cred_flow.run_local_server(port=0)
+    with open(cred_cache, "wb") as cache_stream:
+        pickle.dump(creds, cache_stream)
+    return creds
+
+
+def get_client(conf_path: str, cred_cache: str, refresh_cache: bool = False):
     """Gets a googleapi client"""
-    try:
-        with open(cred_cache, "rb") as cache_stream:
-            creds = pickle.load(cache_stream)
-    except (FileNotFoundError, pickle.UnpicklingError):
-        cred_flow = flow.InstalledAppFlow.from_client_secrets_file(
-            conf_path, ["https://www.googleapis.com/auth/spreadsheets"],
-        )
-        creds = cred_flow.run_local_server(port=0)
-        with open(cred_cache, "wb") as cache_stream:
-            pickle.dump(creds, cache_stream)
+    if refresh_cache:
+        creds = _make_new_client(conf_path, cred_cache)
+    else:
+        try:
+            with open(cred_cache, "rb") as cache_stream:
+                creds = pickle.load(cache_stream)
+        except (FileNotFoundError, pickle.UnpicklingError):
+            creds = _make_new_client(conf_path, cred_cache)
 
     return googleapiclient.discovery.build("sheets", "v4", credentials=creds,)
