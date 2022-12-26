2899a8b717a3fb987b8b7cfd61d572c6c2b5f7b3
yiwen song <yiwen.song@airtable.com>
2022-12-25 20:08:56 -0800

    implement players store

--- avalon/storage/postgres/players.go
+++ avalon/storage/postgres/players.go
@@ -21,25 +21,69 @@ func (s *AvalonPostgresStorage) GetPlayersById(
 	ctx context.Context,
 	playerIds []avalon.PlayerId,
 ) (map[avalon.PlayerId]*avalon.PlayerImpl, error) {
-	return nil, errors.New("Not implemented")
+	query := `
+		SELECT
+			id,
+			name,
+			rating,
+			num_games
+		FROM players
+		WHERE
+			id = ANY($1)`
+
+	players := map[avalon.PlayerId]*avalon.PlayerImpl{}
+	err := s.WithTx(ctx, func(ctx context.Context, tx pgx.Tx) error {
+		rows, err := tx.Query(ctx, query, playerIds)
+		if err != nil {
+			return errors.Wrapf(err, "Query(%q, %+v)", query, playerIds)
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			var entry PlayerEntry
+			err = rows.Scan(
+				&entry.Id,
+				&entry.Name,
+				&entry.Rating,
+				&entry.NumGames,
+			)
+			if err != nil {
+				return errors.Wrapf(err, "rows.Scan")
+			}
+
+			players[avalon.PlayerId(entry.Id.String)] = &avalon.PlayerImpl{
+				Id:       avalon.PlayerId(entry.Id.String),
+				Name:     entry.Name.String,
+				Rating:   entry.Rating.Float64,
+				NumGames: int(entry.NumGames.Int64),
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, errors.Wrapf(err, "WithTx")
+	}
+
+	return players, nil
 }
 
 func (s *AvalonPostgresStorage) GetPlayer(
 	ctx context.Context,
 	playerId avalon.PlayerId,
 ) (player *avalon.PlayerImpl, err error) {
+	query := `
+		SELECT
+			id,
+			name,
+			rating,
+			num_games
+		FROM players
+		WHERE
+			id = $1`
+
 	err = s.WithTx(ctx, func(ctx context.Context, tx pgx.Tx) error {
 		glog.Infof("GetPlayer")
-
-		query := `
-			SELECT
-				id,
-				name,
-				rating,
-				num_games
-			FROM players
-			WHERE
-				id = $1`
 		rows, err := tx.Query(ctx, query, string(playerId))
 		if err != nil {
 			return errors.Wrapf(err, "Query(%s, %s)", query, playerId)
@@ -109,5 +153,34 @@ func (s *AvalonPostgresStorage) UpdatePlayers(
 	ctx context.Context,
 	players []*avalon.PlayerImpl,
 ) error {
-	return errors.New("Not implemented")
+	query := `
+		UPDATE players
+		SET
+			name = $2,
+			rating = $3,
+			num_games = $4
+		WHERE
+			id = $1`
+
+	// Do this for each player
+	return s.WithTx(ctx, func(ctx context.Context, tx pgx.Tx) error {
+		for _, player := range players {
+			ct, err := tx.Exec(
+				ctx,
+				query,
+				player.Id,
+				player.Name,
+				player.Rating,
+				player.NumGames,
+			)
+			if err != nil {
+				return errors.Wrapf(err, "tx.Exec(%q, %+v)", query, player)
+			}
+			if rowsAffected := ct.RowsAffected(); rowsAffected == 0 {
+				return errors.Errorf("Player with id %q was not found", player.Id)
+			}
+		}
+
+		return nil
+	})
 }
--- avalon/storage/postgres/players_test.go
+++ avalon/storage/postgres/players_test.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	context "context"
+	"fmt"
 	testing "testing"
 
 	assert "github.com/stretchr/testify/assert"
@@ -42,4 +43,67 @@ func TestGetPlayer(t *testing.T) {
 
 	fetchedPlayer, err := storage.GetPlayer(ctx, id)
 	assert.Equal(t, fetchedPlayer, createdPlayer)
+}
+
+func TestGetPlayersById(t *testing.T) {
+	ctx := context.Background()
+	storage, cleanup, err := NewAvalonPostgresStorage(ctx, testConnectionString)
+	assert.NoError(t, err)
+	defer cleanup()
+
+	// Make 4 players
+	players := make([]*avalon.PlayerImpl, 4)
+	playerIds := make([]avalon.PlayerId, 4)
+	for idx := range players {
+		players[idx] = avalon.NewPlayer(fmt.Sprintf("player-%d", idx))
+
+		err = storage.CreatePlayer(ctx, players[idx])
+		assert.NoError(t, err)
+
+		playerIds[idx] = players[idx].Id
+	}
+
+	// Get first 3 player ids
+	returnedPlayers, err := storage.GetPlayersById(ctx, playerIds[:3])
+	assert.NoError(t, err)
+	assert.Equal(t, 3, len(returnedPlayers), "expected returned players to be size 3")
+	for _, player := range players[:3] {
+		returnedPlayer, ok := returnedPlayers[player.Id]
+		assert.True(t, ok, "returned players did not contain player with id %q", player.Id)
+		assert.Equal(t, player, returnedPlayer)
+	}
+}
+
+func TestUpdatePlayers(t *testing.T) {
+	ctx := context.Background()
+	storage, cleanup, err := NewAvalonPostgresStorage(ctx, testConnectionString)
+	assert.NoError(t, err)
+	defer cleanup()
+
+	// Make 4 players
+	players := make([]*avalon.PlayerImpl, 4)
+	playerIds := make([]avalon.PlayerId, 4)
+	for idx := range players {
+		players[idx] = avalon.NewPlayer(fmt.Sprintf("player-%d", idx))
+
+		err = storage.CreatePlayer(ctx, players[idx])
+		assert.NoError(t, err)
+
+		playerIds[idx] = players[idx].Id
+	}
+
+	for _, player := range players {
+		player.Name = fmt.Sprintf("player-%s", player.Id)
+		player.Rating = 1241.
+		player.NumGames++
+	}
+
+	err = storage.UpdatePlayers(ctx, players)
+	assert.NoError(t, err)
+
+	playersById, err := storage.GetPlayersById(ctx, playerIds)
+	for _, player := range playersById {
+		assert.Equal(t, 1241., player.Rating)
+		assert.Equal(t, 1, player.NumGames)
+	}
 }
